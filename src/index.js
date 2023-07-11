71dd89a2140884b95b04fb9a23dbba7b4eb9441b
Neat <sophaneat@student.fdu.edu>
2023-07-11 16:32:41 -0700

    Add logrocket

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import LogRocket from 'logrocket';
+LogRocket.init('iu2xs9/portfolio');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
